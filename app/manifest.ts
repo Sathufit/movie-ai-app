01421d905f5785fdbbd841df813d3899eedb34d1
Sathush Nanyakkara <sathufit@gmail.com>
2025-11-09 13:22:04 +0530

    up

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,20 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#9333ea',
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/icon-192.svg',
         sizes: '192x192',
-        type: 'image/png',
+        type: 'image/svg+xml',
       },
       {
-        src: '/icon-512.png',
+        src: '/icon-512.svg',
         sizes: '512x512',
-        type: 'image/png',
+        type: 'image/svg+xml',
+      },
+      {
+        src: '/icon-512.svg',
+        sizes: '512x512',
+        type: 'image/svg+xml',
+        purpose: 'maskable',
       },
     ],
   }
